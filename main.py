7bab1f6ffda6e6ad320a3af663c23546a66bea38
TheCzar <Admin@czarsaparilla.sturmhaus.net>
2021-06-24 16:30:45 -0500

    Added the first name function and tied it into the main function.
    This marks the first time the script will actually execute the initially desired function.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 # main.py - Main Function
+import names
+
+def logo():
+    print('#'*50)
+    print('#\n# Dummy Account Creator')
+    print('# v 0.0.0\n#')
+    print('#'*50)
+
 
 def main():
-    print('Hello World')
+    logo()
+    print("\nThis operation will attempt to create a new dummy account.")
+    # Pull two seemingly random names out of a very large text file.
+    name = names.get_new_name()
+    new_name = name.rsplit(" ")
+    first_name = new_name[0]
+    last_name = new_name[1]
+    user_name = f'{name[0].lower()}{new_name[1].lower()}'
+    print(f'First Name: {first_name}')
+    print(f'Last Name: {last_name}')
+    print(f'Username: {user_name}')
+
 
 main()
--- names.py
+++ names.py
@@ -3,17 +3,15 @@
 import random
 
 
-class Creator:
+def get_new_name():
+    with open("names.txt", "r") as file:
+        lines = file.readlines()
+        lines_end = len(lines)
+        fn = random.randrange(1, lines_end, 1)
+        ln = random.randrange(1, lines_end, 1)
+        first_name = str.rstrip(lines[fn])
+        last_name = str.rstrip(lines[ln])
+        name = f'{first_name} {last_name}'
+        return str(name)
 
-    def __init__(self):
-        print('Names Class')
 
-    def get_name(self):
-        with open("names.txt", "r") as file:
-            lines = file.readlines()
-            lines_end = len(lines)
-            fn = random.randrange(1, lines_end, 1)
-            ln = random.randrange(1, lines_end, 1)
-            first_name = str.rstrip(lines[fn])
-            last_name = str.rstrip(lines[ln])
-            print(f'{first_name} {last_name}')
