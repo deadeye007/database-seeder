18fabc4178125c99b8f5c30e64b66e90852a763b
TheCzar <Admin@czarsaparilla.sturmhaus.net>
2021-11-07 16:07:51 -0600

    ArgParse Addition
    I have created (with the help of the Internet, but does that really need to be stated at this point?) a skeleton upon which to build the rest of the CLI aspects of the program so that the user can specify which parts they want to interact with.

--- main.py
+++ main.py
@@ -1,30 +1,84 @@
 # main.py - Main Function
-import database
-import users
+import argparse
+import sys
+# import database
+# import users
 
 
 def logo():
     print('#'*50)
-    print('#\n# Dummy Database Seeder')
-    print('# v 0.0.4\n#')
+    print('#\n# Dummy Database Seeder and Feeder')
+    print('#')
+    print('# Version 0.0.4\n#')
     print('#'*50)
     print()
 
 
-def main():
-    logo()
+class MainOptions(object):
 
-    # Create database and tables.
-    database.create_db()
-    database.create_table()
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description='Interact directly with the test database.',
+            prog='coolname'
+        )
 
-    # Create customers.
-    users.create()
+        parser.add_argument('command', help='Subcommand to run.')
+        # Limit the length of parse_args to prevent validation issues
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print("Unrecognized Command")
+            parser.print_help()
+            exit(1)
 
-    database.find_random_customer()
-    last = database.last_customer()
+        getattr(self, args.command)()
 
-    print(f"There are currently {last} customers in the database.")
+    def install(self):
+        # TODO: Add the ability to install components individually.
+        parser = argparse.ArgumentParser(
+            description='Attempts to install the database and tables.')
+        args = parser.parse_args(sys.argv[2:])
+        print("Running the installer.")
+        # database.create_db()
+        # database.create_table()
+
+    def find(self):
+        parser = argparse.ArgumentParser(
+            description='Interact directly with the database.')
+        parser.add_argument('--customer')
+        parser.add_argument('--employee')
+        parser.add_argument('--item')
+        args = parser.parse_args(sys.argv[2:4])
+
+        if args.customer:
+            print("Searching for customer: %s" % args.customer)
+
+        if args.employee:
+            print("Searching for employee: %s" % args.employee)
+
+        if args.item:
+            print("Searching for item: %s" % args.item)
+
+    def add(self):
+        parser = argparse.ArgumentParser(
+            description='Add to the database.')
+        parser.add_argument('--customer')
+        parser.add_argument('--employee')
+        parser.add_argument('--item')
+        args = parser.parse_args(sys.argv[2:4])
+        print("This will add something.")
+
+    def delete(self):
+        parser = argparse.ArgumentParser(
+            description='Add to the database.')
+        parser.add_argument('--customer')
+        parser.add_argument('--employee')
+        parser.add_argument('--item')
+        args = parser.parse_args(sys.argv[2:4])
+        print("This will delete something.")
+
+    # TODO: Flesh out the employee aspects.
+    # TODO: Add ability to seed random items.
 
 
-main()
+if __name__ == "__main__":
+    MainOptions()
